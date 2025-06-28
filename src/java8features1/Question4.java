c527aaa801f2f8e66a4c7b0653189b0421ad60f9
PrinceRana8496 <prince.rana@tothenew.com>
2025-06-28 12:12:33 +0530

    modified question4

--- src/java8features1/Question4.java
+++ src/java8features1/Question4.java
@@ -33,8 +33,8 @@ public class Question4 {
 
         // parameter constructor reference
         PersonFactory namedCreator = Person::new;
-        Person john = namedCreator.create("Aman");
-        Person mary = namedCreator.create("Sumit");
+        Person Aman = namedCreator.create("Aman");
+        Person Sumit = namedCreator.create("Sumit");
 
 
     }
